4ba0f5c366cc1a784343632fd8d67f1dc6672ea6
Srihariharasudhan Balakannan <139725379+Srihariharasudhan-Balakannan@users.noreply.github.com>
2023-12-13 01:11:17 +0530

    Update scrape_data.py

--- scrape_data.py
+++ scrape_data.py
@@ -1,21 +1,17 @@
 # imports
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+import shutil
 
 # get driver object
 def get_driver():
-    # return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    driver_path = ChromeDriverManager(version="114.0.5735.90").install()
-    driver = webdriver.Chrome(executable_path=driver_path, options=options)
-    
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')  
-options.add_argument('--disable-dev-shm-usage')
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("user-agent=your_user_agent_string")
+    chrome_options.add_argument('--headless')
+    driver_path = shutil.which('chromedriver')
+    chrome_service = webdriver.ChromeService(executable_path=driver_path)
+    driver = webdriver.Chrome(options=chrome_options, service=chrome_service)
+    return driver
 
 # function to get all the states in India by scraping knowindia.india.gov.in site
 def get_states_indian():
